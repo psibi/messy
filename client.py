80f829187870ba0a9526eb4e6bfba99ddb791ee9
Sibi <sibi@psibi.in>
2012-02-25 18:14:13 +0530

    Added license text

--- client.py
+++ client.py
@@ -1,3 +1,17 @@
+#Author Sibi <sibi@psibi.in>
+#This program is free software: you can redistribute it and/or modify
+#under the terms of the GNU General Public License as published by
+#Free Software Foundation, either version 3 of the License, or
+#your option) any later version.
+#
+#This  program is distributed in the hope that it will be useful,
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#General Public License for more details.
+#
+#should have received a copy of the GNU General Public License
+#with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/python
 import mechanize
 import cookielib
